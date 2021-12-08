bba395e82a544ac22dbe642b5ff81f87a5123899
jakubpetrikcz <jakubpetrikcz@gmail.com>
2021-12-08 20:18:25 +0100

    Initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
 
+  option = {
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 10,
+    //autoplay: true,
+  };
 }
